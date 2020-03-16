c249baebbbc829d5a06029435a567ddc762c34e8
Semyon Kirekov <kirekov@gmail.com>
2020-03-16 21:28:32 +0700

    added json schema to rule

--- tests/lib/rules/jsx-id.js
+++ tests/lib/rules/jsx-id.js
@@ -1,16 +1,14 @@
 "use strict";
 
-const test = require('ava');
-const RuleTester = require('eslint-ava-rule-tester');
+const RuleTester = require('eslint').RuleTester;
 const md5 = require('blueimp-md5');
 const rule = require("../../../lib/rules/jsx-id");
-//const RuleTester = require("eslint").RuleTester;
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester(test, {
+RuleTester.setDefaultConfig({
   parserOptions: {
     ecmaVersion: 6,
     ecmaFeatures: {
@@ -18,6 +16,9 @@ const ruleTester = new RuleTester(test, {
     },
   }
 });
+
+const ruleTester = new RuleTester();
+
 ruleTester.run("jsx-id", rule, {
 
   valid: [
@@ -32,8 +33,19 @@ ruleTester.run("jsx-id", rule, {
         column: 31
       }],
       output: `<Row val1="dsad" val2={23123} qa-id="qa-id_${md5(
-          'Row_val1_dsad,val2_23123,qa-id_null'
-      )}" >SomeValue</Row>`,
+          'Row_val1_"dsad",val2_23123,qa-id_null_false'
+      )}">SomeValue</Row>`,
+    },
+    {
+      code: '<Row val1="dsad" val2={23123} qa-id>SomeValue</Row>',
+      options: [{prefix: 'prefix'}],
+      errors: [{
+        line: 1,
+        column: 31
+      }],
+      output: `<Row val1="dsad" val2={23123} qa-id="prefix_${md5(
+          'Row_val1_"dsad",val2_23123,qa-id_null_false'
+      )}">SomeValue</Row>`,
     }
   ]
 });
