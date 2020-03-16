e45e8a49825fdeb1d0070b4ed9f6de4f7695ef6a
Semyon Kirekov <kirekov@gmail.com>
2020-03-16 22:27:27 +0700

    added tests for jsx-id rule.

--- tests/lib/rules/jsx-id.js
+++ tests/lib/rules/jsx-id.js
@@ -22,30 +22,65 @@ const ruleTester = new RuleTester();
 ruleTester.run("jsx-id", rule, {
 
   valid: [
-    //'<MyComponent val="" isDisabled otherVal={222} />'
+    {
+      code: '<SomeBody val="" not-qa-id hehehe="juu" />',
+    },
+    {
+      code: '<Once qa-id val="" isDisabled otherVal={222} />',
+      options: [{attribute: 'some-id'}]
+    },
+    {
+      code: '<Told some-id val="" strawberry={0.001} />',
+      options: [{attribute: 'qa-id'}]
+    },
+    {
+      code: '<Me qa-id val="" isDisabled otherVal={222} />',
+      options: [{attribute: 'text-id'}]
+    },
   ],
 
   invalid: [
     {
-      code: '<Row val1="dsad" val2={23123} qa-id>SomeValue</Row>',
+      code: '<TheWorld val1="dsad" val2={23123} qa-id>No Value</TheWorld>',
       errors: [{
         line: 1,
-        column: 31
+        column: 36
       }],
-      output: `<Row val1="dsad" val2={23123} qa-id="qa-id_${md5(
-          'Row_val1_"dsad",val2_23123,qa-id_null_false'
-      )}">SomeValue</Row>`,
+      output: `<TheWorld val1="dsad" val2={23123} qa-id="qa-id_${md5(
+          'TheWorld_val1_"dsad",val2_23123,qa-id_null_false'
+      )}">No Value</TheWorld>`,
     },
     {
-      code: '<Row val1="dsad" val2={23123} qa-id>SomeValue</Row>',
+      code: '<IsGonna tt={[1, 2, 3, identifier, [0, 1], {a: 1}]} nn={14} qa-id>SomeValue</IsGonna>',
       options: [{prefix: 'prefix'}],
       errors: [{
         line: 1,
-        column: 31
+        column: 61
       }],
-      output: `<Row val1="dsad" val2={23123} qa-id="prefix_${md5(
-          'Row_val1_"dsad",val2_23123,qa-id_null_false'
-      )}">SomeValue</Row>`,
+      output: `<IsGonna tt={[1, 2, 3, identifier, [0, 1], {a: 1}]} nn={14} qa-id="prefix_${md5(
+          'IsGonna_tt_[1, 2, 3, identifier, [0, 1], OBJECT_EXPRESSION_VALUE],nn_14,qa-id_null_false'
+      )}">SomeValue</IsGonna>`,
+    },
+    {
+      code: '<Roll ut="good" my-id />',
+      options: [{prefix: 'my-id', attribute: 'my-id', divider: '@'}],
+      errors: [{
+        line: 1,
+        column: 17
+      }],
+      output: `<Roll ut="good" my-id="my-id@${md5(
+          'Roll_ut_"good",my-id_null_true'
+      )}" />`,
+    },
+    {
+      code: '<Me textVal="yo man" numVal={69.96} qa-id>Yellow bus</Me>',
+      errors: [{
+        line: 1,
+        column: 37
+      }],
+      output: `<Me textVal="yo man" numVal={69.96} qa-id="qa-id_${md5(
+          'Me_textVal_"yo man",numVal_69.96,qa-id_null_false'
+      )}">Yellow bus</Me>`,
     }
   ]
 });
